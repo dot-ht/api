d43af7088671139745be1d391bbdfb785c1d048d
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2022-03-11 12:18:19 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, abort, Api
 import pymongo
+import requests
 import os
 import json
 
@@ -8,14 +9,26 @@ app = Flask(__name__)
 api = Api(app)
 
 MONGO_CONNECTION = os.getenv('MONGO_CONNECTION')
+API_KEY = os.getenv('API_KEY')
+APOD = os.getenv('APOD')
 
 myclient = pymongo.MongoClient(MONGO_CONNECTION)
 mydb = myclient['planet_db']
 planet_col = mydb['planet_collection']
 
+nasa_api = requests.get(APOD + API_KEY)
+
 class Day(Resource):
     def get(self):
-        return {}
+        return_api_json = nasa_api.json()
+        
+        del return_api_json['date']
+        del return_api_json['service_version']
+        del return_api_json['media_type']
+        del return_api_json['url']
+        
+        #return nasa api json json
+        return return_api_json
 
 class Planets(Resource):
     def get(self):
@@ -33,6 +46,6 @@ class Chat(Resource):
     def get(self):
         return {}
 
-api.add_resource(Planets, '/day/')
+api.add_resource(Day, '/day/')
 api.add_resource(Planets, '/planets/')
 api.add_resource(Chat, '/chat/')
