b8a9948b43e4ceb94e36bed0d9b9b010d098d4f6
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2022-03-11 11:22:20 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,26 +1,8 @@
-from tkinter.tix import Tree
-from unittest import defaultTestLoader
 from flask import Flask, request
 from flask_restful import Resource, abort, Api
-from marshmallow import Schema, fields
 import pymongo
 import os
 import json
-
-planets = {
-    'planet_name': fields.String(allow_none=True),
-    'planet_img': fields.String(allow_none=True),
-    'planet_info': fields.String(allow_none=True)
-}
-
-# content = {
-#     'answer_txt': fields.String(allow_none=True),
-#     'cdn_img_ling': field
-# }
-
-bot_question = {
-    'type': fields.String(allow_none=True),
-}
 
 app = Flask(__name__)
 api = Api(app)
@@ -31,24 +13,16 @@ myclient = pymongo.MongoClient(MONGO_CONNECTION)
 mydb = myclient['planet_db']
 planet_col = mydb['planet_collection']
 
-PlanetSchema = Schema.from_dict(planets)
-BotSchema = Schema.from_dict(bot_question)
-
 class Planets(Resource):
     def get(self):
-        request_data = request.get_json()
         
-        errors = PlanetSchema().validate(request_data)
-        print(errors)
-        if errors:
-            return {"planets": None}, 400
-        
-        
-        
-        mongo_query = planet_col.find()
-        return_json = []
-        for i in mongo_query:
-            return_json.append(i)
+        try:
+            mongo_query = planet_col.find()
+            return_json = []
+            for i in mongo_query:
+                return_json.append(i)
+        except:
+            return {'planets': None}, 400
         
         return {'planets': return_json}, 200
 
